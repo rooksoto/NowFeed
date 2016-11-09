49c9f0a4af2146626a32207ab708e05209228cc1
dannylui91 <dannylui91@gmail.com>
2016-11-09 15:43:16 -0500

    weather card v1

--- app/src/main/java/nyc/c4q/rafaelsoto/nowfeed/MainActivity.java
+++ app/src/main/java/nyc/c4q/rafaelsoto/nowfeed/MainActivity.java
@@ -7,13 +7,16 @@ import android.support.v7.widget.RecyclerView;
 import android.widget.Toast;
 
 import nyc.c4q.rafaelsoto.nowfeed.models.darksky.Forecast;
+import nyc.c4q.rafaelsoto.nowfeed.models.geolocation.GeoLocation;
 import nyc.c4q.rafaelsoto.nowfeed.networks.darksky.DarkSkyClient;
+import nyc.c4q.rafaelsoto.nowfeed.networks.geolocation.GeoLocationClient;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
+    private GeoLocationClient geoLocation;
     private DarkSkyClient darkSkyClient;
 
     @Override
@@ -25,12 +28,31 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(new CardAdapter());
 
-        createWeatherCard();
+        initGeoLocation(); //will also call initWeatherCard within
     }
 
-    public void createWeatherCard() {
+
+    private void initGeoLocation() {
+        geoLocation = GeoLocationClient.getInstance();
+        Call<GeoLocation> call = geoLocation.getLocation();
+        call.enqueue(new Callback<GeoLocation>() {
+            @Override
+            public void onResponse(Call<GeoLocation> call, Response<GeoLocation> response) {
+                GeoLocation location = response.body();
+                String latLon = location.getLat() + ", " + location.getLon();
+                initWeatherCard(latLon);
+            }
+
+            @Override
+            public void onFailure(Call<GeoLocation> call, Throwable t) {
+                Toast.makeText(MainActivity.this, "Error getting location data", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void initWeatherCard(String latLon) {
         darkSkyClient = DarkSkyClient.getInstance();
-        Call<Forecast> call = darkSkyClient.getForecast("70,-40"); //Fixed lat, lon but will revise later
+        Call<Forecast> call = darkSkyClient.getForecast(latLon); //Fixed lat, lon but will revise later
         call.enqueue(new Callback<Forecast>() {
             @Override
             public void onResponse(Call<Forecast> call, Response<Forecast> response) {
@@ -38,6 +60,7 @@ public class MainActivity extends AppCompatActivity {
                 CardAdapter cardAdapter = (CardAdapter) recyclerView.getAdapter(); //Get reference to cardAdapter from the recyclerView
                 cardAdapter.addToDataList(forecast); //Add the Forecast object to the data list in the adapter (first card)
                 cardAdapter.addToDataList(new User("JJ")); //Example of adding another object to the data list (another card)
+
             }
 
             @Override
--- app/src/main/java/nyc/c4q/rafaelsoto/nowfeed/viewholders/WeatherViewHolder.java
+++ app/src/main/java/nyc/c4q/rafaelsoto/nowfeed/viewholders/WeatherViewHolder.java
@@ -1,24 +1,61 @@
 package nyc.c4q.rafaelsoto.nowfeed.viewholders;
 
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+
+import java.text.SimpleDateFormat;
 
 import nyc.c4q.rafaelsoto.nowfeed.R;
 import nyc.c4q.rafaelsoto.nowfeed.models.darksky.Forecast;
+
+import static com.bumptech.glide.gifdecoder.GifHeaderParser.TAG;
 
 /**
  * Created by dannylui on 10/31/16.
  */
 public class WeatherViewHolder extends RecyclerView.ViewHolder {
     private final View mView;
+    private TextView detailView;
+    private ImageView currentIconView;
     private TextView currentTempView;
+
+    private TextView dayOneView;
+    private TextView dayTwoView;
+    private TextView dayThreeView;
+    private TextView dayFourView;
+    private TextView dayFiveView;
+
+    private ImageView dayOneIconView;
+    private ImageView dayTwoIconView;
+    private ImageView dayThreeIconView;
+    private ImageView dayFourIconView;
+    private ImageView dayFiveIconView;
+
+    private TextView dayOneHiView;
+    private TextView dayTwoHiView;
+    private TextView dayThreeHiView;
+    private TextView dayFourHiView;
+    private TextView dayFiveHiView;
+
+    private TextView dayOneLoView;
+    private TextView dayTwoLoView;
+    private TextView dayThreeLoView;
+    private TextView dayFourLoView;
+    private TextView dayFiveLoView;
+
+
     public WeatherViewHolder(ViewGroup parent) {
         super(inflateView(parent));
         mView = itemView;
-        currentTempView = (TextView) mView.findViewById(R.id.current_temp_view);
+        initViews();
+
     }
 
     private static View inflateView(ViewGroup parent) {
@@ -28,7 +65,146 @@ public class WeatherViewHolder extends RecyclerView.ViewHolder {
 
     public void bind(Object data) {
         Forecast forecastData = (Forecast) data;
-        String currentTemp = String.valueOf(forecastData.getCurrently().getTemperature());
+
+        //Detail
+        String summary = forecastData.getCurrently().getSummary();
+        String windSpeed = String.valueOf(Math.round(forecastData.getCurrently().getWindSpeed()));
+        String precipitation = String.valueOf((int)(forecastData.getCurrently().getPrecipProbability() * 100));
+        detailView.setText(summary + "  Wind " + windSpeed + "m/h  Precip  " + precipitation + "%");
+
+        //Current temp icon
+        String currentIcon = forecastData.getCurrently().getIcon();
+        handleIcons(currentIcon, currentIconView);
+
+
+        //Current temp
+        String currentTemp = Math.round(forecastData.getCurrently().getTemperature()) + "°";
         currentTempView.setText(currentTemp);
+
+        //Days of the week
+        String dayOne = unixTsToDay(forecastData.getDaily().getData().get(1).getTime());
+        String dayTwo = unixTsToDay(forecastData.getDaily().getData().get(2).getTime());
+        String dayThree = unixTsToDay(forecastData.getDaily().getData().get(3).getTime());
+        String dayFour = unixTsToDay(forecastData.getDaily().getData().get(4).getTime());
+        String dayFive = unixTsToDay(forecastData.getDaily().getData().get(5).getTime());
+
+        dayOneView.setText(dayOne);
+        dayTwoView.setText(dayTwo);
+        dayThreeView.setText(dayThree);
+        dayFourView.setText(dayFour);
+        dayFiveView.setText(dayFive);
+
+        //Icons for each day
+        String dayOneIcon = forecastData.getDaily().getData().get(1).getIcon();
+        String dayTwoIcon = forecastData.getDaily().getData().get(2).getIcon();
+        String dayThreeIcon = forecastData.getDaily().getData().get(3).getIcon();
+        String dayFourIcon = forecastData.getDaily().getData().get(4).getIcon();
+        String dayFiveIcon = forecastData.getDaily().getData().get(5).getIcon();
+
+        handleIcons(dayOneIcon, dayOneIconView);
+        handleIcons(dayTwoIcon, dayTwoIconView);
+        handleIcons(dayThreeIcon, dayThreeIconView);
+        handleIcons(dayFourIcon, dayFourIconView);
+        handleIcons(dayFiveIcon, dayFiveIconView);
+
+        //Highs for each day
+        String dayOneHi = Math.round(forecastData.getDaily().getData().get(1).getTemperatureMax()) + "°";
+        String dayTwoHi = Math.round(forecastData.getDaily().getData().get(2).getTemperatureMax()) + "°";
+        String dayThreeHi = Math.round(forecastData.getDaily().getData().get(3).getTemperatureMax()) + "°";
+        String dayFourHi = Math.round(forecastData.getDaily().getData().get(4).getTemperatureMax()) + "°";
+        String dayFiveHi = Math.round(forecastData.getDaily().getData().get(5).getTemperatureMax()) + "°";
+
+        dayOneHiView.setText(dayOneHi);
+        dayTwoHiView.setText(dayTwoHi);
+        dayThreeHiView.setText(dayThreeHi);
+        dayFourHiView.setText(dayFourHi);
+        dayFiveHiView.setText(dayFiveHi);
+
+        //Lows for each day
+        String dayOneLo = Math.round(forecastData.getDaily().getData().get(1).getTemperatureMin()) + "°";
+        String dayTwoLo = Math.round(forecastData.getDaily().getData().get(2).getTemperatureMin()) + "°";
+        String dayThreeLo = Math.round(forecastData.getDaily().getData().get(3).getTemperatureMin()) + "°";
+        String dayFourLo = Math.round(forecastData.getDaily().getData().get(4).getTemperatureMin()) + "°";
+        String dayFiveLo = Math.round(forecastData.getDaily().getData().get(5).getTemperatureMin()) + "°";
+
+        dayOneLoView.setText(dayOneLo);
+        dayTwoLoView.setText(dayTwoLo);
+        dayThreeLoView.setText(dayThreeLo);
+        dayFourLoView.setText(dayFourLo);
+        dayFiveLoView.setText(dayFiveLo);
+    }
+
+    public String unixTsToDay(int unixTs) {
+        java.util.Date date = new java.util.Date((long)unixTs*1000);
+        SimpleDateFormat dateFormatter = new SimpleDateFormat("E");
+        return dateFormatter.format(date);
+
+    }
+
+    public void initViews() {
+        detailView = (TextView) mView.findViewById(R.id.detail_view);
+        currentIconView = (ImageView) mView.findViewById(R.id.current_icon_view);
+        currentTempView = (TextView) mView.findViewById(R.id.current_temp_view);
+
+        dayOneView = (TextView) mView.findViewById(R.id.day_one_view);
+        dayTwoView = (TextView) mView.findViewById(R.id.day_two_view);
+        dayThreeView = (TextView) mView.findViewById(R.id.day_three_view);
+        dayFourView = (TextView) mView.findViewById(R.id.day_four_view);
+        dayFiveView = (TextView) mView.findViewById(R.id.day_five_view);
+
+        dayOneIconView = (ImageView) mView.findViewById(R.id.day_one_icon_view);
+        dayTwoIconView = (ImageView) mView.findViewById(R.id.day_two_icon_view);
+        dayThreeIconView = (ImageView) mView.findViewById(R.id.day_three_icon_view);
+        dayFourIconView = (ImageView) mView.findViewById(R.id.day_four_icon_view);
+        dayFiveIconView = (ImageView) mView.findViewById(R.id.day_five_icon_view);
+
+        dayOneHiView = (TextView) mView.findViewById(R.id.day_one_hi_view);
+        dayTwoHiView = (TextView) mView.findViewById(R.id.day_two_hi_view);
+        dayThreeHiView = (TextView) mView.findViewById(R.id.day_three_hi_view);
+        dayFourHiView = (TextView) mView.findViewById(R.id.day_four_hi_view);
+        dayFiveHiView = (TextView) mView.findViewById(R.id.day_five_hi_view);
+
+        dayOneLoView = (TextView) mView.findViewById(R.id.day_one_lo_view);
+        dayTwoLoView = (TextView) mView.findViewById(R.id.day_two_lo_view);
+        dayThreeLoView = (TextView) mView.findViewById(R.id.day_three_lo_view);
+        dayFourLoView = (TextView) mView.findViewById(R.id.day_four_lo_view);
+        dayFiveLoView = (TextView) mView.findViewById(R.id.day_five_lo_view);
+    }
+
+    public void handleIcons(String iconText, ImageView iconView) {
+        switch(iconText) {
+            case "clear-day":
+                Glide.with(mView.getContext()).load(R.drawable.skycon_clear_day).into(iconView);
+                break;
+            case "clear-night":
+                Glide.with(mView.getContext()).load(R.drawable.skycon_clear_night).into(iconView);
+                break;
+            case "rain":
+                Glide.with(mView.getContext()).load(R.drawable.skycon_rain).into(iconView);
+                break;
+            case "snow":
+                Glide.with(mView.getContext()).load(R.drawable.skycon_snow).into(iconView);
+                break;
+            case "sleet":
+                Glide.with(mView.getContext()).load(R.drawable.skycon_sleet).into(iconView);
+                break;
+            case "wind":
+                Glide.with(mView.getContext()).load(R.drawable.skycon_wind).into(iconView);
+                break;
+            case "fog":
+                Glide.with(mView.getContext()).load(R.drawable.skycon_fog).into(iconView);
+                break;
+            case "cloudy":
+                Glide.with(mView.getContext()).load(R.drawable.skycon_cloudy).into(iconView);
+                break;
+            case "partly-cloudy-day":
+                Glide.with(mView.getContext()).load(R.drawable.skycon_partly_cloudy_day).into(iconView);
+                break;
+            case "partly-cloudy-night":
+                Glide.with(mView.getContext()).load(R.drawable.skycon_party_cloudy_night).into(iconView);
+                break;
+            default:
+                Log.d(TAG, "handleIcons: " + iconText + iconView.toString());
+        }
     }
 }
